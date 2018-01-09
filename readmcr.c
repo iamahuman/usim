0d83696327aa61d62c2ea7cdacba3ec366022778
Alfred M. Szmidt <ams@gnu.org>
2018-01-09 19:23:39 +0100

    readmcr.c: Minor cleanup.

--- readmcr.c
+++ readmcr.c
@@ -1,179 +1,175 @@
-#include "usim.h"
+// readmcr --- dump a microcode (MCR) file
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <inttypes.h>
 
+#include "usim.h"
 #include "ucode.h"
 #include "misc.h"
 
-int showmcr;
-int debug;
-int needswap;
+bool showmcr;
+bool debug;
+bool needswap;
+int skip;
 
-ucw_t ucode[16 * 1024];
-
-int
+void
 dump_i_mem(int fd, int start, int size)
 {
-	int i;
 	int loc;
 
+	printf("i-memory; start %o, size %o\n", start, size);
 	loc = start;
-	for (i = 0; i < size; i++) {
-		unsigned int w1;
-		unsigned int w2;
-		unsigned int w3;
-		unsigned int w4;
-		unsigned long long ll;
+	for (int i = 0; i < size; i++) {
+		uint16_t w1;
+		uint16_t w2;
+		uint16_t w3;
+		uint16_t w4;
+		ucw_t ll;
 
 		w1 = read16(fd);
 		w2 = read16(fd);
 		w3 = read16(fd);
 		w4 = read16(fd);
 		ll =
-			((unsigned long long) w1 << 48) |
-			((unsigned long long) w2 << 32) |
-			((unsigned long long) w3 << 16) |
-			((unsigned long long) w4 << 0);
+			((ucw_t) w1 << 48) |
+			((ucw_t) w2 << 32) |
+			((ucw_t) w3 << 16) |
+			((ucw_t) w4 << 0);
 
 		if (showmcr)
-			printf("%03o %016Lo\n", loc, ll);
+			printf("%03o %016" PRIo64 "\n", loc, ll);
 
-		ucode[loc] = ll;
 		loc++;
 	}
-	return 0;
 }
 
-int
+void
 dump_d_mem(int fd, int start, int size)
 {
-	int i;
-
-	for (i = 0; i < size; i++) {
+	printf("d-memory; start %o, size %o\n", start, size);
+	for (int i = 0; i < size; i++) {
 		read16(fd);
 		read16(fd);
 	}
-
-	return 0;
 }
 
-int
+void
 dump_a_mem(int fd, int start, int size)
 {
-	int i;
-	unsigned int v;
-
 	printf("a-memory; start %o, size %o\n", start, size);
-	for (i = 0; i < size; i++) {
+	for (int i = 0; i < size; i++) {
+		uint32_t v;
+
 		v = read32(fd);
+
 		if ((i >= 0347 && i <= 0400) |
 		    (i >= 0600 && i <= 0610) |
 		    (i < 010)) {
-			printf("%o <- %o\n", i, v);
+			printf("%o <- %" PRIo32 "\n", i, v);
 		}
 	}
-
-	return 0;
 }
 
-int
+void
 dump_main_mem(int fd, int start, int size)
 {
+	printf("main-memory; start %d, size %d\n", start, size);
 	read32(fd);
-	printf("start %d, size %d\n", start, size);
 	lseek(fd, 0, SEEK_CUR);
-	return 0;
 }
 
 void
 usage(void)
 {
-	fprintf(stderr, "usage: readmcr FILE>\n");
+	fprintf(stderr, "usage: readmcr FILE\n");
 	fprintf(stderr, "dump a microcode file\n");
 	fprintf(stderr, "\n");
 	fprintf(stderr, "  -b             swap bytes\n");
 	fprintf(stderr, "  -d             extra debug info\n");
 	fprintf(stderr, "  -s N           skip N * 32-bit values\n");
-	exit(1);
+	fprintf(stderr, "  -m             show microcode\n");
 }
-
-extern char *optarg;
-extern int optind;
 
 int
 main(int argc, char *argv[])
 {
 	int c;
 	int fd;
-	int done;
-	int skip;
+	bool done;
 
-	showmcr = 0;
-	needswap = 1;
-	skip = 0;
+	showmcr = false;
+	needswap = true;
+	skip = false;
 
-	while ((c = getopt(argc, argv, "mbds:")) != -1) {
+	while ((c = getopt(argc, argv, "bds:mh")) != -1) {
 		switch (c) {
 		case 'b':
-			needswap = 0;
+			needswap = false;
 			break;
 		case 'd':
-			debug++;
+			debug = true;
 			break;
 		case 's':
 			skip = atoi(optarg);
 			break;
 		case 'm':
-			showmcr = 1;
+			showmcr = true;
 			break;
+		case 'h':
+			usage();
+			exit(0);
+		default:
+			usage();
+			exit(1);
 		}
 	}
+	argc -= optind;
+	argv += optind;
 
-	if (optind >= argc)
+	if (argc != 1) {
 		usage();
+		exit(1);
+	}
 
-	fd = open(argv[optind], O_RDONLY);
-	if (fd) {
+	fd = open(argv[0], O_RDONLY);
+	if (fd == -1) {
+		fprintf(stderr, "%s: no such file or directory\n", argv[0]);
+		exit(1);
+	}
+
+	if (skip) {
+		while (skip--)
+			read32(fd);
+	}
+
+	done = false;
+	while (!done) {
 		int code;
 		int start;
 		int size;
 
-		if (skip) {
-			while (skip--)
-				read32(fd);
-		}
+		code = read32(fd);
+		start = read32(fd);
+		size = read32(fd);
 
-		done = 0;
-		while (!done) {
-			code = read32(fd);
-			start = read32(fd);
-			size = read32(fd);
-
-			printf("code %d, start %o, size %o\n", code, start, size);
-
-			switch (code) {
-			case 1:
-				printf("i-memory\n");
-				dump_i_mem(fd, start, size);
-				break;
-			case 2:
-				printf("d-memory\n");
-				dump_d_mem(fd, start, size);
-				break;
-			case 3:
-				printf("main-memory\n");
-				dump_main_mem(fd, start, size);
-				break;
-			case 4:
-				printf("a-memory\n");
-				dump_a_mem(fd, start, size);
-				done = 1;
-				break;
-			}
+		switch (code) {
+		case 1:
+			dump_i_mem(fd, start, size);
+			break;
+		case 2:
+			dump_d_mem(fd, start, size);
+			break;
+		case 3:
+			dump_main_mem(fd, start, size);
+			break;
+		case 4:
+			dump_a_mem(fd, start, size);
+			done = true;
+			break;
 		}
 	}
 
