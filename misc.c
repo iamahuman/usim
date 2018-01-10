288e21a3050e1c5dbd2bb07a80a8d65a7d2b9ef1
Alfred M. Szmidt <ams@gnu.org>
2018-01-10 19:35:32 +0100

    misc.c, misc.h: Minor cleanup.

--- misc.c
+++ misc.c
@@ -1,3 +1,5 @@
+// misc.c --- ---!!!
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
--- misc.h
+++ misc.h
@@ -1,10 +1,15 @@
-unsigned int read16(int fd);
-unsigned int read32(int fd);
+#ifndef USIM_MISC_H
+#define USIM_MISC_H
 
-unsigned long str4(char *s);
-char *unstr4(unsigned long s);
+extern unsigned int read16(int fd);
+extern unsigned int read32(int fd);
+
+extern unsigned long str4(char *s);
+extern char *unstr4(unsigned long s);
 
 #define BLOCKSZ (256 * 4)
 
-int read_block(int fd, int block_no, unsigned char *buf);
-int write_block(int fd, int block_no, unsigned char *buf);
+extern int read_block(int fd, int block_no, unsigned char *buf);
+extern int write_block(int fd, int block_no, unsigned char *buf);
+
+#endif
