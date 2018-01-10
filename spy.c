804faac227e6c0ad94ad9d6333ac55a2cf8443de
Alfred M. Szmidt <ams@gnu.org>
2018-01-10 19:35:51 +0100

    spy.c, spy.h: Minor cleanup.

--- spy.c
+++ spy.c
@@ -1,3 +1,5 @@
+// spy.c --- ---!!!
+
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
--- spy.h
+++ spy.h
@@ -1,3 +1,9 @@
-void spy_init(char *port);
-void spy_unibus_read(int offset, unsigned int *pv);
-void spy_unibus_write(int offset, unsigned int v);
+#ifndef USIM_SPY_H
+#define USIM_SPY_H
+
+extern void spy_init(char *port);
+
+extern void spy_unibus_read(int offset, unsigned int *pv);
+extern void spy_unibus_write(int offset, unsigned int v);
+
+#endif
